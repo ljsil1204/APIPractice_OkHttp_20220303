91e318dd064c7557f3ebb3d7613d29060f5752a5
이진실 <ljsil1204@naver.com>
2022-03-03 16:21:09 +0900

    binding 변수 이용 -> 이벤트 처리 진행   - 실제 API 서버 연동 예정

--- app/src/main/java/com/neppplus/apipractice_okhttp_20220303/MainActivity.kt
+++ app/src/main/java/com/neppplus/apipractice_okhttp_20220303/MainActivity.kt
@@ -2,12 +2,35 @@ package com.neppplus.apipractice_okhttp_20220303
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.neppplus.apipractice_okhttp_20220303.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        setupEvents()
+        setValues()
+    }
 
+    fun setupEvents() {
+        binding.btnLogin.setOnClickListener {
+
+//            id / pw 추출
+            val inputId = binding.edtId.text.toString()
+            val inputPw = binding.edtPassword.text.toString()
+
+//            API 서버에 아이디 / 비번을 보내서 실제로 회원인지 검사 => 로그인 시도
+
+
+        }
+    }
+
+    fun setValues() {
 
     }
+
 }
