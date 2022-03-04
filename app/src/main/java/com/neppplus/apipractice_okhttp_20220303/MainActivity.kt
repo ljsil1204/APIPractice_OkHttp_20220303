e4703ad76b459dcf520af8dd074cc2c120119ed8
이진실 <ljsil1204@naver.com>
2022-03-04 15:14:20 +0900

    메인화면도 BaseActivity 물려받음

--- app/src/main/java/com/neppplus/apipractice_okhttp_20220303/MainActivity.kt
+++ app/src/main/java/com/neppplus/apipractice_okhttp_20220303/MainActivity.kt
@@ -8,7 +8,7 @@ import com.neppplus.apipractice_okhttp_20220303.databinding.ActivityMainBinding
 import com.neppplus.apipractice_okhttp_20220303.utils.ServerUtil
 import org.json.JSONObject
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity() {
 
     lateinit var binding : ActivityMainBinding
 
@@ -19,7 +19,7 @@ class MainActivity : AppCompatActivity() {
         setValues()
     }
 
-    fun setupEvents() {
+    override fun setupEvents() {
         binding.btnLogin.setOnClickListener {
 
 //            id / pw 추출
@@ -65,7 +65,7 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    fun setValues() {
+    override fun setValues() {
 
     }
 
