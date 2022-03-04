c6824c9f4cc0b8335afc61b859b8c4ef95413d21
이진실 <ljsil1204@naver.com>
2022-03-04 10:54:26 +0900

    본문 String (한글 깨짐) > JSONObject 로 변환, 한글 복구확인

--- app/src/main/java/com/neppplus/apipractice_okhttp_20220303/utils/ServerUtil.kt
+++ app/src/main/java/com/neppplus/apipractice_okhttp_20220303/utils/ServerUtil.kt
@@ -2,6 +2,7 @@ package com.neppplus.apipractice_okhttp_20220303.utils
 
 import android.util.Log
 import okhttp3.*
+import org.json.JSONObject
 import java.io.IOException
 
 class ServerUtil {
@@ -60,7 +61,14 @@ class ServerUtil {
 //                    응답 : response 변수 > 응답의 본문 (body) 만 보자
 
                     val bodyString = response.body!!.string() // toString으로 하면 안됨! , string() 기능은 1회용 -> 변수에 담아두고 이용.
-                    Log.d("서버 테스트", bodyString)
+
+//                    응답의 본문을 String으로 변환하면, JSON Encoding 적용된 상태 (한글 깨짐)
+//                    JSONObject 객체로 응답 본문 String 을 변환해주면, 한글이 복구됨.
+//                      => UI에서도 JSONObject를 이용해서, 데이터 추출 / 실제 활용
+
+                    val jsonObj = JSONObject( bodyString )
+
+                    Log.d("서버 테스트", jsonObj.toString())
 
                 }
 
