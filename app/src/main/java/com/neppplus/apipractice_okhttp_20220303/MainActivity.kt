9ffd38c39e51c30c49d4ff2bcc36b41338675da8
이진실 <ljsil1204@naver.com>
2022-03-07 17:04:24 +0900

    JSONArray의 JSONObject를 하나씩 변수에 (반복으로) 담아서 > 로그로 출력

--- app/src/main/java/com/neppplus/apipractice_okhttp_20220303/MainActivity.kt
+++ app/src/main/java/com/neppplus/apipractice_okhttp_20220303/MainActivity.kt
@@ -2,6 +2,7 @@ package com.neppplus.apipractice_okhttp_20220303
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.databinding.DataBindingUtil
 import com.neppplus.apipractice_okhttp_20220303.databinding.ActivityMainBinding
 import com.neppplus.apipractice_okhttp_20220303.datas.TopicData
@@ -44,6 +45,18 @@ class MainActivity : BaseActivity() {
                 val topicsArr = dataObj.getJSONArray("topics")
                 
 //                topicsArr 내부를 하나씩 추출 (JSONObject {} ) => TopicData()로 변환
+
+//                JSONArray는 for-each 문법 지원 x. (차후 : ArrayList의 for-each 활용 예정)
+//                JAVA : for (int i=0; i< 배열.length; i++)
+                for ( i in 0 until topicsArr.length()) {
+
+//                    []  => {}, {}, {} ,... 순서에 맞는 {}를 변수에 담자
+//                    JSON 파싱의  {} => (JSONArray에게서) JSONObject 추출.
+                    val topicObj = topicsArr.getJSONObject(i)
+
+                    Log.d("받아낸 주제", topicObj.toString())
+
+                }
 
             }
 
