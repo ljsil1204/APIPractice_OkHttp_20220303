5f1b600e5e52e6fe26c3321d7cc4ba23fb30126f
이진실 <ljsil1204@naver.com>
2022-03-07 09:41:49 +0900

    메인액티비티  이름 변경 > 로그인 액티비티

--- app/src/main/java/com/neppplus/apipractice_okhttp_20220303/LoginActivity.kt
+++ app/src/main/java/com/neppplus/apipractice_okhttp_20220303/LoginActivity.kt
@@ -1,21 +1,20 @@
 package com.neppplus.apipractice_okhttp_20220303
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import androidx.databinding.DataBindingUtil
-import com.neppplus.apipractice_okhttp_20220303.databinding.ActivityMainBinding
+import com.neppplus.apipractice_okhttp_20220303.databinding.ActivityLoginBinding
 import com.neppplus.apipractice_okhttp_20220303.utils.ServerUtil
 import org.json.JSONObject
 
-class MainActivity : BaseActivity() {
+class LoginActivity : BaseActivity() {
 
-    lateinit var binding : ActivityMainBinding
+    lateinit var binding : ActivityLoginBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_login)
         setupEvents()
         setValues()
     }
