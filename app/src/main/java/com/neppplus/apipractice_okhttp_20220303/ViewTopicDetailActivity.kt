d1e682f8f36dc3563d4025ab24782970ae4b5ab5
이진실 <ljsil1204@naver.com>
2022-03-08 12:15:22 +0900

    초기 집입 / 상세 조회 API 모두, 같은 코드를 실행하도록 UI 세팅 코드 함수화

--- app/src/main/java/com/neppplus/apipractice_okhttp_20220303/ViewTopicDetailActivity.kt
+++ app/src/main/java/com/neppplus/apipractice_okhttp_20220303/ViewTopicDetailActivity.kt
@@ -30,11 +30,18 @@ class ViewTopicDetailActivity : BaseActivity() {
 
     override fun setValues() {
 
-        binding.txtTitle.text = mTopicData.title
-        Glide.with(mContext).load(mTopicData.imageURL).into(binding.imgTopicBackground)
-
+        setTopicDataToUi()
         getTopicDetailFromServer()
 
+    }
+
+    fun setTopicDataToUi(){
+
+//        토론 주제에 대한 데이터들은, UI에 반영하는 함수
+//        화면 초기 진입 실행 + 서버에서 다시 받아왔을 때도 실행.
+
+        binding.txtTitle.text = mTopicData.title
+        Glide.with(mContext).load(mTopicData.imageURL).into(binding.imgTopicBackground)
     }
 
     fun getTopicDetailFromServer() {
@@ -51,6 +58,9 @@ class ViewTopicDetailActivity : BaseActivity() {
 //                변환된 객체를, mTopicData로 다시 대입 => UI 반영도 다시 실행
                 mTopicData = topicData
 
+                runOnUiThread {
+                    setTopicDataToUi()
+                }
 
             }
 
